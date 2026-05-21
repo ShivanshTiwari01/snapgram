64255ce5a9a577ff155ec9af370430d7f448bc9d
Shivansh Tiwari <shivanshtiwari2014@gmail.com>
2026-05-21 19:46:06 +0530

    initial commit

---
+++ backend/src/auth/dto/register.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsString, MinLength, IsNotEmpty } from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password!: string;
+}
